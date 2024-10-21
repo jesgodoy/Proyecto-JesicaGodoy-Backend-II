90fb89bc1a90d9d3161d6ba07a5d885e86c39962
jesgodoy <jesicagodoy7@gmail.com>
2024-10-21 00:54:29 -0300

    Middleware para autenticar usuarios

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,19 +1,24 @@
 import passport from 'passport';
 
+// Middleware para autenticar usuarios
 export const authenticateUser = passport.authenticate('current', { session: false });
 
+// Middleware para solo admin
 export function soloAdmin(req, res, next) {
-    if(req.user.role === "admin") {
-        next();
+    console.log(req.user); // Verifica el contenido del usuario autenticado
+    if (req.user && req.user.role === "admin") {
+        return next();
     } else {
-        res.status(403).send("Acceso denegado, este lugar es solo para admin queridooo");
+        return res.status(403).send("Acceso denegado, este lugar es solo para admin");
     }
 }
 
+// Middleware para solo usuarios
 export function soloUser(req, res, next) {
-    if(req.user.role === "user") {
-        next();
+    console.log(req.user); // Verifica el contenido del usuario autenticado
+    if (req.user && req.user.role === "user") {
+        return next();
     } else {
-        res.status(403).send("Acceso denegado, este lugar es solo para usuarios comunachos");
+        return res.status(403).send("Acceso denegado, este lugar es solo para usuarios");
     }
 }
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import productController from "../controllers/product.controller.js";
 import cartController from "../controllers/cart.controllers.js";
-
+import { authenticateUser, soloAdmin, soloUser } from '../middlewares/auth.middleware.js'; 
 const router = Router();
 
 router.get("/", async (req, res) => {
@@ -15,6 +15,15 @@ router.get("/", async (req, res) => {
     }
 });
 
+router.get('/admin', authenticateUser, soloAdmin, (req, res) => {
+    res.send('Bienvenido a la zona admin');
+});
+
+// Ruta para la zona de usuarios
+router.get('/user', authenticateUser, soloUser, (req, res) => {
+    res.send('Bienvenido a la zona de usuarios');
+});
+
 router.get("/products", productController.renderProducts);
 
 router.get('/products/:pid', productController.renderDetailProduct);
@@ -23,7 +32,8 @@ router.get('/products/:pid', productController.renderDetailProduct);
 router.get('/carts/:cid', cartController.renderCart);
 
 
-router.get("/realtimeproducts", async (req, res) => {
+// Ruta para productos en tiempo real
+router.get("/realtimeproducts", authenticateUser, soloAdmin, async (req, res) => {
     try {
         res.render("realtimeproducts");
     } catch (error) {
