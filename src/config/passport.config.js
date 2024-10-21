06fa7a01f61c6f1e8d162b7fb2e3b1fa5830b842
jesgodoy <jesicagodoy7@gmail.com>
2024-10-21 02:06:34 -0300

    correcion passport handlebars

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -27,51 +27,6 @@ const initializePassport = () => {
         }
     }));
 
-   
-
-    passport.use(new GitHubStrategy({
-        clientID: "Iv23lipSxx9TPyEArYgR",
-        clientSecret: "8c72fafc05c94e4800564b63377791a99ae582f6",
-        callbackURL: "http://localhost:8080/api/sessions/githubcallback"
-    }, async (accessToken, refreshToken, profile, done) => {
-        
-
-        try {
-            let user = await UserModel.findOne({ email: profile._json.email });
-
-            if (!user) {
-                const newCart = await cartManager.addCart(); 
-                let newUser = {
-                    first_name: profile._json.name,
-                    last_name: "",
-                    age: 37,
-                    email: profile._json.email, 
-                    password: "",
-                    cart: newCart._id
-                };
-
-                let result = await UserModel.create(newUser);
-                done(null, result); 
-            } else {
-                done(null, user); 
-            }
-
-        } catch (error) {
-            return done(error);
-        }
-    }));
-    passport.serializeUser((user, done) => {
-        done(null, user.id); 
-    });
-
-    passport.deserializeUser(async (id, done) => {
-        try {
-            const user = await UserModel.findById(id);
-            done(null, user);
-        } catch (error) {
-            done(error);
-        }
-    });
 };
 
 export default initializePassport;
